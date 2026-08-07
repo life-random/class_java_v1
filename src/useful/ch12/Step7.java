e05f51e688dbc0ddaa8c92dd6b0eb4d3d65118d0
morningMoka <random0815@naver.com>
2026-08-07 12:49:49 +0900

    second commit

--- src/useful/ch12/Step7.java
+++ src/useful/ch12/Step7.java
@@ -3,9 +3,14 @@ package useful.ch12;
 public class Step7 {
     public static void main(String[] args) {
 
+        long start = System.currentTimeMillis();
+
         int sum  = 0;
         for (int i = 1; i <= 100_000_000; i++) {
             sum += i;
         }
+
+        long end = System.currentTimeMillis();
+        System.out.println("걸린 시간: " + (end - start) + " ms");
     }
 }
