271bad2cc42e21d5bccd5ca01ad1b1009f68c0ec
morningMoka <random0815@naver.com>
2026-08-07 14:16:50 +0900

    step7 작성

--- src/useful/ch12/Step7.java
+++ src/useful/ch12/Step7.java
@@ -1,16 +1,21 @@
 package useful.ch12;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 public class Step7 {
     public static void main(String[] args) {
 
-        long start = System.currentTimeMillis();
-
+        LocalTime start = LocalTime.now();
         int sum  = 0;
         for (int i = 1; i <= 100_000_000; i++) {
             sum += i;
         }
 
-        long end = System.currentTimeMillis();
-        System.out.println("걸린 시간: " + (end - start) + " ms");
+        LocalTime end = LocalTime.now();
+        Duration work = Duration.between(start, end);
+        System.out.println("합계 : " +  sum);
+        System.out.println("걸린 시간: " + work.toMillis() + " ms");
+        System.out.println("걸린 시간: " + work.toSeconds() + " 초");
     }
 }
