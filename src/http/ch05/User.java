c62786a3f9feb30e2ef617183a00b5cff41e5238
morningMoka <random0815@naver.com>
2026-08-21 09:29:05 +0900

    todo writer

---
+++ src/http/ch05/User.java
@@ -0,0 +1,40 @@
+package http.ch05;
+
+import lombok.Data;
+
+
+@Data
+public class User {
+
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+    private Address address;
+
+    private String phone;
+    private String website;
+    private Company company;
+
+    @Data
+    public static class Address {
+        private String street;
+        private String suite;
+        private String city;
+        private String zipcode;
+        private Geo geo;
+
+        @Data
+        public static class Geo {
+            private String lat;
+            private String lng;
+        }
+    }
+
+    @Data
+    public static class Company {
+        private String name;
+        private String catchPhrase;
+        private String bs;
+    }
+}
---
+++ src/http/ch06/Todo.java
@@ -0,0 +1,15 @@
+package http.ch06;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+    private int userId;
+    private int id;
+    private String title;
+    private boolean completed;
+}
